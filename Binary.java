e9197c53cafe617da1eb03910773d49eebc6ab72
praveenkumarl713 <praveenkumarpraveen713@gmail.com>
2022-02-16 13:06:18 +0530

    Created Binary.java

---
+++ Binary.java
@@ -0,0 +1,24 @@
+import java.util.Scanner;
+
+public class Binary {
+
+	void convertAndCount(int num) {
+
+		int count = 0;
+		String binary = Integer.toBinaryString(num);
+		for (int i = 0; i < binary.length(); i++)
+			if (binary.charAt(i) == '1')
+
+				count++;
+
+		System.out.println(count);
+	}
+
+	public static void main(String[] args) {
+		Scanner scan = new Scanner(System.in);
+		Binary obj = new Binary();
+		int n = scan.nextInt();
+
+		obj.convertAndCount(n);
+	}
+}
